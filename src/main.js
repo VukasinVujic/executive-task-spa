74aa03accc3f4c173fbb394e01414b2872bd5bd3
VukasinVujic <vukasinvujic021@gmail.com>
2020-09-23 12:42:59 +0200

    fist commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-Vue.config.productionTip = false
+library.add(faUserSecret);
+// FontAwesomeIcon
+Vue.component("font-awesome-icon");
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: "#app",
+  components: { App },
+  template: "<App/>",
+});
